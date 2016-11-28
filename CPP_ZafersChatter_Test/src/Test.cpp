fb57c9d3234e8e915331a45c53cdaec12db537f1
DESKTOP-3M9COAN\Zmote <shodaime.hikarikage@gmail.com>
2016-11-28 20:06:52 +0100

    First tests

--- CPP_ZafersChatter_Test/src/Test.cpp
+++ CPP_ZafersChatter_Test/src/Test.cpp
@@ -2,15 +2,47 @@
 #include "ide_listener.h"
 #include "xml_listener.h"
 #include "cute_runner.h"
+#include "zChatterUI.h"
+#include "zChatterUI.cpp"
+#include "chat_ui.cpp"
 
-void thisIsATest() {
-	ASSERTM("start writing tests", false);	
+void testDefaultsLoadedForSessionHost() {
+	zChatterUI zcui{NULL};
+	zcui.loadDefaults();
+	ASSERT(!zcui.get_session_host().IsEmpty());
+}
+
+void testDefaultsLoadedForSessionHostPort() {
+	zChatterUI zcui{NULL};
+	zcui.loadDefaults();
+	ASSERT(!zcui.get_session_host_port().IsEmpty());
+}
+
+void testDefaultsLoadedForSessionName() {
+	zChatterUI zcui{NULL};
+	zcui.loadDefaults();
+	ASSERT(!zcui.get_session_name().IsEmpty());
+}
+
+void testDefaultsLoadedForSessionOtherHost() {
+	zChatterUI zcui{NULL};
+	zcui.loadDefaults();
+	ASSERT(!zcui.get_session_other_host().IsEmpty());
+}
+
+void testDefaultsLoadedForSessionOtherHostPort() {
+	zChatterUI zcui{NULL};
+	zcui.loadDefaults();
+	ASSERT(!zcui.get_session_other_host_port().IsEmpty());
 }
 
 bool runAllTests(int argc, char const *argv[]) {
 	cute::suite s { };
-	//TODO add your test here
-	s.push_back(CUTE(thisIsATest));
+	s.push_back(CUTE(testDefaultsLoadedForSessionHost));
+	s.push_back(CUTE(testDefaultsLoadedForSessionHostPort));
+	s.push_back(CUTE(testDefaultsLoadedForSessionName));
+	s.push_back(CUTE(testDefaultsLoadedForSessionOtherHost));
+	s.push_back(CUTE(testDefaultsLoadedForSessionOtherHostPort));
 	cute::xml_file_opener xmlfile(argc, argv);
 	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
 	auto runner { cute::makeRunner(lis, argc, argv) };
